a87892b4f2ca20a26606e19810cb7dbbc83eb09d
whyroke <2803001950@qq.com>
2021-04-13 19:33:06 +0800

    完成了炸弹和方块的移动轨迹绘制

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,13 +1,49 @@
 <template>
   <div class="home">
-    <block />
-    <bomb />
-    <piano
-    :position="state.position"
-    :bottom="state.bottom"
-    :left="state.left"
-    :transform="state.transform"
+    <div class="blockTrack">
+       <span>
+         <block />
+       </span>
+       <span>
+          <block />
+       </span>
+       <span>
+          <block />
+       </span>
+       <span>
+          <block />
+       </span>
+       <span>
+          <block />
+       </span>
+       <span>
+          <block />
+       </span>
+       <span>
+          <block />
+       </span>
+    </div>
+    <div class="bombTrack">
+       <span>
+         <bomb />
+       </span>
+       <span>
+         <bomb />
+       </span>
+       <span>
+         <bomb />
+       </span>
+       <span>
+         <bomb />
+       </span>
+       <span>
+         <bomb />
+       </span>
+    </div>
+    <div class="content">
+      <piano
      />
+    </div>
   </div>
 </template>
 
@@ -24,9 +60,11 @@ export default {
     bomb
   },
   setup () {
+    const blockList = [1, 2, 3, 4, 5, 6, 7]
+    const bombList = [1, 2, 3, 4, 5]
     const state = reactive({
       position: 'absolute;',
-      bottom: '10px;',
+      bottom: '17px;',
       left: '50%;',
       transform: 'translate(-50%);'
     })
@@ -45,4 +83,63 @@ export default {
    height:100vh;
    background:pink;
 }
+.content{
+  position:absolute;
+  bottom:10px;
+  left:50%;
+  transform: translate(-50%);
+}
+.blockTrack,.bombTrack{
+  display:inline-block;
+  position:absolute;
+  top:0;
+  bottom:21rem;
+  left:50%;
+  transform:translate(-50%);
+}
+.blockTrack{
+ & span{
+  display:inline-block;
+  width:2.7rem;
+  animation:blockRun 10s infinite;
+}
+}
+@keyframes blockRun {
+   0%{
+     transform: translateY(0px);
+   }
+   10%{
+     transform:translateY(200px)
+   }
+   20%{
+     transform:translateY(300px);
+   }
+   30%{
+     transform:translateY(500px);
+   }
+ }
+.bombTrack{
+ & span {
+   display:inline-block;
+   width:2.9rem;
+ }
+ & span:nth-child(2){
+   margin-right:2.5rem;
+    animation:bombRun 10s infinite;
+ }
+}
+@keyframes bombRun {
+   0%{
+     transform: translateY(0px);
+   }
+   10%{
+     transform:translateY(250px)
+   }
+   20%{
+     transform:translateY(370px);
+   }
+   30%{
+     transform:translateY(500px);
+   }
+ }
 </style>
