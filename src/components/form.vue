a7792b1a641d0a85d73d0f4433d6122dc0137107
whyroke <2803001950@qq.com>
2021-04-27 16:50:02 +0800

    封装了ajax请求

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,48 +1,73 @@
 <template>
-    <input placeholder="填写账号名称" class="username"/>
-    <br />
-    <input placeholder="输入密码" class="password" />
-    <br />
-    <button class="btn">注册</button>
+  <input placeholder="填写账号名称" class="username" v-model="state.username" />
+  <br />
+  <input placeholder="填写邮箱" class="email" v-model="state.email" />
+  <br />
+  <input placeholder="输入密码" class="password" v-model="state.password" />
+  <br />
+  <button class="btn" @click="handleClick">注册</button>
 </template>
 
 <script>
+import { reactive } from 'vue'
+import { ajax } from '../request'
 export default {
-  name: 'form'
+  name: 'form',
+  setup () {
+    const state = reactive({
+      email: '',
+      username: '',
+      password: ''
+    })
+
+    const handleClick = () => {
+      ajax({
+        url: 'http://localhost:5000/login',
+        type: 'POST',
+        data: state
+      })
+    }
+    return { handleClick, state }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 @import '../style/variable.scss';
- input, button {
-     border:none;
-     outline:none;
-     background:none;
- }
-.username, .password{
-    width:19rem;
-    padding:3px 7px;
-    margin-bottom:11px;
-    border-bottom:3px solid $themColor1;
-    color:$themColor1;
+input,
+button {
+  border: none;
+  outline: none;
+  background: none;
 }
-.username::placeholder, .password::placeholder {
-    color:$themColor1;
+.username,
+.password,
+.email {
+  width: 19rem;
+  padding: 3px 7px;
+  margin-bottom: 11px;
+  border-bottom: 3px solid $themColor1;
+  color: $themColor1;
+}
+.username::placeholder,
+.password::placeholder,
+.email::placeholder {
+  color: $themColor1;
 }
 .btn {
-    width:20rem;
-    height:30px;
-    margin-top:10px;
-    font-size:15px;
-    font-weight: bloder;
-    border: 3px solid $themColor1;
-    border-radius:5px;
-    color:$themColor1;
+  width: 20rem;
+  height: 30px;
+  margin-top: 10px;
+  font-size: 15px;
+  font-weight: bloder;
+  border: 3px solid $themColor1;
+  border-radius: 5px;
+  color: $themColor1;
 }
-.btn:hover, .btn:focus{
-    box-shadow: 1px 1px 1px rgb(139, 139, 139);
-    background:$themColor1;
-    color:#fff;
-
+.btn:hover,
+.btn:focus {
+  box-shadow: 1px 1px 1px rgb(139, 139, 139);
+  background: $themColor1;
+  color: #fff;
 }
 </style>
---
+++ src/request.js
@@ -0,0 +1,58 @@
+function ajax (
+  options
+) {
+  options = {
+    type: options.type || 'get',
+    url: options.url || '',
+    data: options.data || {},
+    header: options.header || { 'Content-Type': 'application', Authorization: '' }
+  }
+  // 创建ajax对象
+  const xhr = new XMLHttpRequest()
+  xhr.withCredenticals = true
+  // 拼接请求参数的变量
+
+  let params = ''
+  for (const attr in options.data) {
+    params += attr + '=' + options.data[attr] + '&'
+  }
+  // 将参数最后的&截取掉
+  params = params.substr(0, params.length - 1)
+  if (options.type === 'get') {
+    options.url = options.url + '?' + params
+  }
+  // 配置ajax对象
+  xhr.open(options.type, options.url)
+  console.log('header', options)
+  xhr.setRequestHeader('Authorization', options.header.Authorization)
+  if (options.type === 'post') {
+    const contentType = options.header['Content-Type']
+    // 设置请求类型格式
+    // console.log(contentType)
+    xhr.setRequestHeader('Content-Type', contentType)
+    if (contentType === 'application/json') {
+      // console.log("yes",options.data)
+      xhr.send(JSON.stringify(options.data))
+    } else {
+      xhr.send(params)
+    }
+  } else {
+    // 发送请求
+    xhr.send()
+  }
+
+  // 监听xhr对象下面的onload事件
+  // 当xhr对象接收完响应数据后触发
+  xhr.onload = function () {
+    console.log(xhr.status)
+    if (xhr.status === 200) {
+      options.success(xhr.responseText)
+    } else {
+      options.error(xhr.responseText, xhr)
+    }
+  }
+}
+
+module.exports = {
+  ajax
+}
